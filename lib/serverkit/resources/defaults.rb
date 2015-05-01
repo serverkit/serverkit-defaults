5e9efa11e104d0120102a4be7affe6ccded5d48f
Takehiro Adachi <takehiro0740@gmail.com>
2015-05-01 10:22:10 +0900

    Add Serverkit::Resources::Defaults#host attribute

--- lib/serverkit/resources/defaults.rb
+++ lib/serverkit/resources/defaults.rb
@@ -9,6 +9,9 @@ module Serverkit
     class Defaults < Base
       DEFAULT_DOMAIN = "NSGlobalDomain"
 
+      # @exmaple "current"
+      attribute :host, type: String
+
       # @example "com.apple.TextEdit"
       attribute :domain, default: DEFAULT_DOMAIN, type: String
 
@@ -19,7 +22,7 @@ module Serverkit
       attribute :value, required: true, type: [Array, Fixnum, Float, Hash, String]
 
       def apply
-        run_command("defaults write #{escaped_domain} #{escaped_key} #{type_option} #{value_in_plist}")
+        run_command("defaults #{host_option} write #{escaped_domain} #{escaped_key} #{type_option} #{value_in_plist}")
       end
 
       # @note Override
@@ -89,6 +92,17 @@ module Serverkit
       # @return [String]
       def value_in_plist
         ::Shellwords.shellescape(Plist.generate(value))
+      end
+
+      # @return [String]
+      def host_option
+        case host
+        when "current"
+          "-currentHost"
+        when nil
+        else
+          "-host #{host}"
+        end
       end
 
       # @return [String]
