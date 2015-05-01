bb4b228f7385776d2af46bf3559a35b73b35ac5f
Takehiro Adachi <takehiro0740@gmail.com>
2015-05-01 13:50:32 +0900

    Enable to pass defaults command's type option explicitly
    check spec for use case

--- lib/serverkit/resources/defaults.rb
+++ lib/serverkit/resources/defaults.rb
@@ -15,11 +15,20 @@ module Serverkit
       # @example "com.apple.keyboard.fnState"
       attribute :key, required: true, type: String
 
+      # @example "-array"
+      attribute :type_option, type: String
+
       # @example 1
       attribute :value, required: true, type: [Array, Fixnum, Float, Hash, String]
 
       def apply
-        run_command("defaults write #{escaped_domain} #{escaped_key} #{type_option} #{value_in_plist}")
+        if type_option
+          run_command("defaults write #{escaped_domain} #{escaped_key} #{type_option} " \
+                      "'#{value}'")
+        else
+          run_command("defaults write #{escaped_domain} #{escaped_key} #{estimated_type_option} " \
+                      "#{value_in_plist}")
+        end
       end
 
       # @note Override
@@ -92,7 +101,7 @@ module Serverkit
       end
 
       # @return [String]
-      def type_option
+      def estimated_type_option
         case value
         when Fixnum
           "-int"
--- spec/serverkit/resources/defaults_spec.rb
+++ spec/serverkit/resources/defaults_spec.rb
@@ -2,13 +2,31 @@ require "spec_helper"
 
 RSpec.describe Serverkit::Resources::Defaults do
   describe "#apply" do
+    let(:recipe) { Serverkit::Recipe.new({}) }
+
     context "with Array #value" do
-      let(:recipe) { Serverkit::Recipe.new({}) }
       let(:key) { "persistent-apps" }
       let(:attributes) { { "key" => key, "value" => [] } }
       let(:resource) { described_class.new(recipe, attributes) }
       let(:command) do
         "defaults write #{described_class::DEFAULT_DOMAIN} #{key}  \\(\\)"
+      end
+
+      it "runs the correct command" do
+        expect(resource).to receive(:run_command).with(command)
+
+        resource.apply
+      end
+    end
+
+    context "with #type_option passed" do
+      let(:key) { "com.apple.keyboard.modifiermapping.1452-601-0" }
+      let(:value) { "<dict><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict>" }
+      let(:type_option) { "-array" }
+      let(:attributes) { { "key" => key, "type_option" => type_option, "value" => value } }
+      let(:resource) { described_class.new(recipe, attributes) }
+      let(:command) do
+        "defaults write #{described_class::DEFAULT_DOMAIN} #{key} #{type_option} '#{value}'"
       end
 
       it "runs the correct command" do
